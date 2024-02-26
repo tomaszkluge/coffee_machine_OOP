25e97bac6ab3cef45581473213ad6fc94d8eb65c
tomaszkluge <tomasz.kluge@gmail.com>
2024-02-26 23:29:42 +0100

    finished while loop for the coffee machine

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
@@ -7,9 +7,15 @@ coffee_maker = CoffeeMaker()
 menu = Menu()
 is_on = True
 
-coffee_maker.report()
-money_machine.report()
-
 while is_on:
     options = menu.get_items()
     choice = input(f"What would you like to drink? ({options}) ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
